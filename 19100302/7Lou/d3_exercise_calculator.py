8604d801d059738a17ac72747de0990088e13223
shining <3310612297@qq.com>
2019-11-11 19:57:12 +0800

    Update d3_exercise_calculator.py

--- 19100302/7Lou/d3_exercise_calculator.py
+++ 19100302/7Lou/d3_exercise_calculator.py
@@ -1,10 +1,10 @@
 #加减乘除计算器
-# 思路：分三步输入 计算 输出 
+# 思路：分三步，输入 计算 输出 
 #输入 input()函数
 #计算 + - * /,提供可选择项
 #输出 print()函数
-x = input('x:')
-y = input('y:')
+x = input('请输入x:')
+y = input('请输入y:')
 z = input('请选择+ or - or * or /:')
 if z == '+':
     print(float(x)+float(y))
