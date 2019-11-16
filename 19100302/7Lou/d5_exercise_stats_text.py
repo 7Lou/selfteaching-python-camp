9e291433b0fc83295c322060a6920879c2492c74
shining <3310612297@qq.com>
2019-11-17 01:49:05 +0800

    Update d5_exercise_stats_text.py

--- 19100302/7Lou/d5_exercise_stats_text.py
+++ 19100302/7Lou/d5_exercise_stats_text.py
@@ -21,18 +21,22 @@ If the implementation is hard to explain, it's a bad idea.
 If the implementation is easy to explain, it may be a good idea.
 Namespaces are one honking great idea -- let's do more of those!
 '''
-import string
-#替换掉标点符号
-for i in text:
-    if i in string.punctuation:  #如果字符是标点符号的话就将其替换为无
-        text = text.replace(i,"")
 
+#如果字符是标点符号的话就将其替换为无
+pc = ".?!,:...;-()[]{}''""'*"
+for i in text:
+    if i in pc:  
+        text = text.replace(i,"")
+print(text)
+#以空格切割字符串为列表    
 text=text.split()
+#以字典统计各个单词在文本中出现的次数
 d={}
 for i in text:
     if i not in d:
         d[i]=1
     else:
         d[i]+=1
+#采用d.items()排序，得到包含键，值的元组的列表；这里对排序的规则进行了定义，x指元组，x[1]是值，x[0]是键；reverse=True为降序
 print(sorted(d.items(),key=lambda x:x[1],reverse=True))
 
